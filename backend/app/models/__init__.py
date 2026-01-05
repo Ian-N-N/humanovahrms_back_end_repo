7fca9ebc68a71b463bfff6b46125313cf2852677
Austine-john <ochiengaustine582@gmail.com>
2026-01-05 10:19:58 +0300

    refactored backend structure

--- backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.extensions import db
-from .role import Role
-from .department import Department
-from .user import User
-from .employee import Employee
-
-__all__ = ["db", "Role", "Department", "User", "Employee"]
--- backend/app/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-# app/routes/__init__.py
-from .auth import auth_bp
-from .users import users_bp
-from .departments import departments_bp
-from .upload import upload_bp
-
-__all__ = ["auth_bp", "users_bp", "departments_bp", "upload_bp"]
